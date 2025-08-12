43c6b2a0e3e322a2bdeb692afc3521e520217e8e
Raditya Rizqan Fadhila <BAI20250016@brins.co.id>
2025-08-12 07:42:28 +0700

    Whatsapp Link

--- resources/views/homepage/car_grid.blade.php
+++ resources/views/homepage/car_grid.blade.php
@@ -26,7 +26,7 @@
             </a>
             {{-- Tawaran --}}
             <a href="https://wa.me/6287784281500?text={{ urlencode('Halo Rafi Nabil, saya tertarik dengan mobil Toyota ' . $mobil->nama_mobil . '. Bisa dibantu info lebih lanjut? Terima kasih.') }}"
-            class="w-full text-center py-2.5 rounded-md border border-red-600 text-red-600 text-sm font-medium hover:bg-red-50 transition duration-150">
+            class="w-full text-center py-2.5 rounded-md border border-red-600 text-red-600 text-sm font-medium hover:bg-red-50 transition duration-150" target="_blank">
                 Minta Penawaran
             </a>
 
